d40a9d86c1996c67ff8bb0492025015858cb2922
Glitch (healthy-crocus-surf) <none>
2020-04-23 16:50:30 +0000

    03_Query Parameters

--- server.js
+++ server.js
@@ -9,21 +9,38 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+var listToDos =  [
+      {id: 1, name: 'Đi Chợ'},
+      {id: 2, name: 'Nấu Cơm'},
+      {id: 3, name: 'Rửa Bát'},
+      {id: 4, name: 'Học code tại CodersX'}  
+    ];
+
+
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
   response.send("I love CodersX");
 });
 
 app.get("/todos", function(req, res) {
+  
+  var q = req.query.q;
+  
+  if(q){
+     var matchToDo = listToDos.filter(function(todo){
+      return todo.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    });
+      res.render('index',{
+        listToDos: matchToDo
+      });
+  }
+    
   res.render('index',{
-    listToDos: [
-      {id: 1, name: 'Đi Chợ'},
-      {id: 2, name: 'Nấu Cơm'},
-      {id: 3, name: 'Rửa Bát'},
-      {id: 4, name: 'Học code tại CodersX'}  
-    ]
+    listToDos: listToDos
   });
 });
+
+
 // listen for requests :)
 app.listen(process.env.PORT, () => {
   console.log("Server listening on port " + process.env.PORT);
